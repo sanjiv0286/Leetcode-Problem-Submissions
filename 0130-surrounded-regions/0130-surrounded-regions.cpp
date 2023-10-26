7df21df46fd2a44c8191ecf7a83fe8357340d645
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-10-26 14:41:55 +0530

    Time: 11 ms (56.92%), Space: 10.4 MB (68.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,53 +1,99 @@
-class Solution {
+class Solution
+{
 public:
-   void solve(vector<vector<char>>& board) {
-    if (board.empty() || board[0].empty()) {
-    // if (board.empty()) {
+    void convert(vector<vector<char>> &board)
+    {
+        int n = board.size();
+        int m = board[0].size();
 
-        return;
-    }
-
-    int rows = board.size();
-    int cols = board[0].size();
-
-    // Mark boundary 'O' cells
-    for (int i = 0; i < rows; i++) {
-        dfs(board, i, 0);
-        dfs(board, i, cols - 1);
-    }
-
-    for (int j = 0; j < cols; j++) {
-        dfs(board, 0, j);
-        dfs(board, rows - 1, j);
-    }
-
-    // Capture surrounded regions
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (board[i][j] == 'O') {
-                board[i][j] = 'X';
-            } 
-            else if (board[i][j] == '#') {
-                board[i][j] = 'O';
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (board[i][j] == 'B')
+                {
+                    board[i][j] = 'O';
+                }
+                else if (board[i][j] == 'O')
+                {
+                    board[i][j] = 'X';
+                }
             }
         }
     }
-}
-private : 
-void dfs(vector<vector<char>>& board, int i, int j) {
-    int rows = board.size();
-    int cols = board[0].size();
 
-    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] != 'O') {
-        return;
+    bool isValid(int i, int j, int n, int m, vector<vector<char>> &board)
+    {
+        if (i >= 0 && i < n && j >= 0 && j < m && board[i][j] == 'O')
+        {
+            return true;
+        }
+        return false;
     }
 
-    board[i][j] = '#';
+    void dfs(vector<vector<char>> &board, int i, int j, int n, int m)
+    {
+        board[i][j] = 'B';
 
-    dfs(board, i - 1, j);
-    dfs(board, i + 1, j);
-    dfs(board, i, j - 1);
-    dfs(board, i, j + 1);
-}
+        if (isValid(i + 1, j, n, m, board))
+        {
+            dfs(board, i + 1, j, n, m);
+        }
+        if (isValid(i - 1, j, n, m, board))
+        {
+            dfs(board, i - 1, j, n, m);
+        }
+        if (isValid(i, j + 1, n, m, board))
+        {
+            dfs(board, i, j + 1, n, m);
+        }
+        if (isValid(i, j - 1, n, m, board))
+        {
+            dfs(board, i, j - 1, n, m);
+        }
+    }
 
+    void solve(vector<vector<char>> &board)
+    {
+        int n = board.size();
+        int m = board[0].size();
+
+        for (int i = 0; i < n; i++)
+        {
+
+            // left -> top bottom
+            int j = 0;
+            if (board[i][j] == 'O')
+            {
+                dfs(board, i, j, n, m);
+            }
+
+            j = m - 1;
+            // right -> top bootm
+            if (board[i][j] == 'O')
+            {
+                dfs(board, i, j, n, m);
+            }
+        }
+
+        for (int j = 0; j < m; j++)
+        {
+
+            // top -> left right
+            int i = 0;
+            if (board[i][j] == 'O')
+            {
+                dfs(board, i, j, n, m);
+            }
+
+            i = n - 1;
+            // bottom -> left right
+            if (board[i][j] == 'O')
+            {
+                dfs(board, i, j, n, m);
+            }
+        }
+
+        convert(board);
+    }
 };
