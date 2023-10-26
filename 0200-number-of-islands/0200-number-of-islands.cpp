b9a7399ca498d427f7500fab8103cebcdda4396a
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-10-26 10:59:18 +0530

    Time: 27 ms (75.72%), Space: 12.8 MB (59.28%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty()) {
+            return 0;
+        }
+
+        int numIslands = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+private:
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col] == '0') {
+            return;
+        }
+
+        grid[row][col] = '0'; // Mark the current cell as visited
+
+        // Explore the neighboring cells
+        dfs(grid, row - 1, col); // Up
+        dfs(grid, row + 1, col); // Down
+        dfs(grid, row, col - 1); // Left
+        dfs(grid, row, col + 1); // Right
+    }
+};
