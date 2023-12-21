a7b9b75cf98c0f68756380e525fcf0e2df1407f6
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-12-21 10:59:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (38.98%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int ct =0;
+       for(int i=1;i<=n;i++){
+           if(n%i==0){
+               ct++;
+           }
+       }
+       return (ct==3);
+    }
+};
