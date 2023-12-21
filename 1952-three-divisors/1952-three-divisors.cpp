0b39c8a83ceddddd6a98b75e336e87bdaf2dc5d2
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-12-21 11:39:54 +0530

    Time: 3 ms (38.29%), Space: 6.2 MB (98.62%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
    bool isPrime(int num ){
-        for(int i=2;i<=num/2;i++){
+        for(int i=2;i*i<=num;i++){
             if(num%i==0){
                 return false;
             }
