c257524389c564f49ceb5d465afb252dc31059e8
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-12-21 11:39:35 +0530

    Time: 2 ms (43.53%), Space: 6.3 MB (75.62%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+   bool isPrime(int num ){
+        for(int i=2;i<=num/2;i++){
+            if(num%i==0){
+                return false;
+            }
+            
+        }
+        return true;
+        
+      }
     bool isThree(int n) {
-        int ct =0;
-       for(int i=1;i<=n;i++){
-           if(n%i==0){
-               ct++;
-           }
-       }
-       return (ct==3);
+        if(n==1||n==2||n==3){
+            return false;
+         }
+        int root = sqrt(n);
+        return ((root*root==n)&& isPrime(root));
+        
     }
 };
