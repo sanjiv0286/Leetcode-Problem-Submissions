2c99b32b92b82de7389c0d1c3891cc0ab1f597b3
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-01-01 11:50:30 +0530

    Time: 20 ms (15.51%), Space: 12.5 MB (54.82%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            cout<<nums[i]<<" ";
+        }
+      
+
+        
+    }
+};
