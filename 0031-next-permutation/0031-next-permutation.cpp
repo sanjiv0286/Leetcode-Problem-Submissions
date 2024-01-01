b32e6a8143286866da84f0bd2044146bd9d2a525
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-01-01 21:07:00 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (54.82%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            cout<<nums[i]<<" ";
+    void nextPermutation(vector<int>& a) {
+        int n=a.size();
+        int gola_index=-1;
+        for(int i=n-1;i>0;i--){
+            if(a[i]>a[i-1]){
+                gola_index=i-1;
+                break;
+            }
         }
-      
+       if(gola_index!=-1) {
+            int swap_index=-1;
+        for(int j=n-1;j>gola_index;j--){
+            if(a[j]>a[gola_index]){
+                swap_index=j;
+                break;
+            }
+        }
+        swap(a[gola_index],a[swap_index]);
+        }
+        reverse(a.begin()+gola_index+1,a.end());
 
         
     }
