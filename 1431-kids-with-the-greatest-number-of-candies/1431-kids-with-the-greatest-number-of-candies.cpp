bacfcde2e1973d066b48e4a0f7a6b02203add8ad
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-01-20 08:22:49 +0530

    Time: 6 ms (15.36%), Space: 10.9 MB (13.83%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& c, int ec) {
         int n = c.size();
-        int maxi = *max_element(c.begin(), c.end());
+        // int maxi = *max_element(c.begin(), c.end());
+        int maxi = c[0];
+        for(int i=1;i<n;i++){
+            if(c[i]>=maxi){
+                maxi = c[i];
+            }
+        }
         vector<bool> tfarr;
         for (int i = 0; i < n; i++) {
-            if (c[i] + ec >= maxi) {
-                tfarr.push_back(true);
-            } else {
-                tfarr.push_back(false);
-            }
+            // if (c[i] + ec >= maxi) {
+            //     tfarr.push_back(true);
+            // } else {
+            //     tfarr.push_back(false);
+            // }
+             tfarr.push_back(c[i] + ec >= maxi);
         }
         return tfarr;
     }
