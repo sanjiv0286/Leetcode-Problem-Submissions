fe36828031ec6dded7313a2ff767a3aa36cf87f9
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-02-02 22:48:34 +0530

    Time: 125 ms (99.45%), Space: 119.6 MB (39.01%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -4,15 +4,14 @@ public:
         sort(v.begin(), v.end());
         vector<vector<int>> res;
         int n = v.size();
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < n - 2; i+=3) {
             if (abs(v[i] - v[i + 2]) <= k) {
                 res.push_back({v[i], v[i + 1], v[i + 2]});
             } else {
                 return {};
             }
-            i += 2;
+            // i += 2;
         }
-
         return res;
         //    else{
         //        return {};
