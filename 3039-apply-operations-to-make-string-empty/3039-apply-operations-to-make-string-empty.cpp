2dfdbbb76ff2a2493323cffc457534bf57dcd8aa
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-02-17 22:58:37 +0530

    Time: 390 ms (25.00%), Space: 32 MB (25.00%) - LeetHub

--- 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.cpp
+++ 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.cpp
@@ -1,71 +1,27 @@
-// class Solution {
-// public:
-//     string lastNonEmptyString(string s) {
-        
-//         map<int,int>m;
-//         for(auto &x:s){
-//             m[x]++;
-//         }
-//         int mini=INT_MAX;
-//         for(auto &x:m){
-//             mini = min(mini,x.second);
-//         }
-//         string t="";
-//         if(mini ==1){
-//             return s;
-//         }
-//         for(auto &x:s){
-//             // while(m[x]!=mini){
-//             //     t+=x;
-//             //     m[x]--;
-//             // }
-//             if(m[x]>mini){
-//                 t+=x;
-//                 m[x]--;
-//             }
-
-//         }
-//         return t;
-        
-//     }
-// };
-
-
 class Solution {
 public:
-  string lastNonEmptyString(string s) {
-    map<char, int> mp, mp2;
-    
-    for (auto ch : s)
-        mp[ch]++;
-    
-    int mx = 0;
-    for (auto [x, y] : mp) {
-        if (y > mx)
-            mx = y;
-    }
-    // cout << mx << endl;
-    
-    string ans = "";
-    int n = s.size();
-    for (auto x : mp) {
-        if (x.second == mx)
-            ans += x.first;
-    }
-    int cnt = ans.size();
-    string res = "";
-    for (auto x : ans)
-        mp2[x]++;
-    
-    for (int i = n - 1; i >= 0; i--) {
-        if (ans.find(s[i]) != string::npos && cnt > 0 && mp2[s[i]] > 0) {
-            res += s[i];
-            cnt--;
-            mp2[s[i]]--;
+    string lastNonEmptyString(string s) {
+        int n=s.size();
+        map<char,int>m;
+        for(auto &x:s){
+            m[x]++;
         }
+        int maxi = INT_MIN;
+        for(auto &x:m){
+            maxi = max(maxi,x.second);
+        }
+        string t="";
+        // if(mini ==1){
+        //     return s;
+        // }
+        for(int i = n-1;i>=0;i-- ){
+            char x =s[i];
+            if(m[x]==maxi){
+                t = x+t;
+                m[x]--;
+            }
+        }
+        return t;
+        
     }
-    reverse(res.begin(), res.end());
-    return res;
-}
-
 };
