0da2b616e55d116865934baf64dd32ba4a0c94d1
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-03-09 21:44:17 +0530

    Time: 14 ms (38.62%), Space: 14.6 MB (18.00%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,15 +1,26 @@
 
-
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end())){
-            ans.push_back(nums);
+    vector<vector<int>> res;
+    void solve(vector<int>& nums, int i) {
+        if (i == nums.size()) {
+            res.push_back(nums);
+            return;
         }
-
-        return ans;
+        unordered_set<int> s;
+        for (int j = i; j < nums.size(); j++) {
+            if (s.find(nums[j]) != s.end()) {
+                continue;
+            }
+            s.insert(nums[j]);      // Mark nums[j] as used at position i
+            swap(nums[i], nums[j]); // Swap elements to generate permutation
+            solve(nums, i + 1);     // Recur for the next position
+            swap(nums[i], nums[j]); // Backtrack: restore the original array
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        solve(nums, 0); // Start generating permutations from index 0
+        return res;
     }
 };
