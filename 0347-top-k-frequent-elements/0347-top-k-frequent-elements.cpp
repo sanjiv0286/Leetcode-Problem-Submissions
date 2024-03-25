27b4bcdade35ffb78992bb1894e2e0c9adfbf977
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-03-25 11:20:15 +0530

    Time: 8 ms (88.14%), Space: 17.2 MB (62.06%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,21 +1,44 @@
+
+// time complexity 
+// brute force : O(nlogn)
+// here : O(nlogk)
+// space : O(n+k)
 class Solution {
 public:
+    typedef pair<int, int> p;
+    // template <typename T>
+    // using min_heap = priority_queue<T, vector<T>, greater<T>>;
+    // Now you can declare a min heap of pairs of ints by writing:
+    // min_heap<pair<int, int>> q;
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int>res;
-        unordered_map<int,int>m;
-        for(auto &x:nums){
+        // 1. map
+        unordered_map<int, int> m;
+        for (auto& x : nums) {
             m[x]++;
         }
-        vector<pair<int,int>>p(m.begin(),m.end());
-        // lamda function in c++
-        sort(p.begin(),p.end(),[](const pair<int,int>&a,const pair<int,int>&b){
-            return a.second >b.second;
-        });
-        
-        for(int i=0;i<k&&i<p.size();i++){
-             res.push_back(p[i].first);    
+
+        // 2. min heap
+        priority_queue<p, vector<p>, greater<p>> pq;
+        // 3. push element in min heap
+        // and maintain size of k only
+        for(auto &x:m){
+            int first = x.first;
+            int second = x.second;
+            // understand push first second value in pq beacause pop easily acc. to key 
+            pq.push({second,first});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        // 4.result 
+        while(!pq.empty()){
+            //   res.push_back(pq.top().first);
+            res.push_back(pq.top().second);//access second value
+            pq.pop();
         }
         return res;
-        
+
     }
 };
