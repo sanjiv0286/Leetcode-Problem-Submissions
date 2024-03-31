5ac6fc4b62ccfbf05e959b16a5f8eedfddee1377
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-03-31 14:32:08 +0530

    Time: 2 ms (80.00%), Space: 7.1 MB (20.00%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.cpp
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int sumOfTheDigitsOfHarshadNumber(int x) {
-        int sum = 0;
-        int n = x;
-        while (x != 0) {
-            sum += x % 10;
-            x /= 10;
-        }
-        if (n % sum == 0) {
+        int sum = x%10+(x/10)%10+(x/100)%10;
+        // int n = x;
+        // while (x != 0) {
+        //     sum += x % 10;
+        //     x /= 10;
+        // }
+        
+        if (x % sum == 0) {
             return sum;
         }
         else
