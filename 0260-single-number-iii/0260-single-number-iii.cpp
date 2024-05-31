4c9e0aac4495a36b86a99ab1ab6575bb635bbcc5
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-01 01:50:40 +0530

    Time: 10 ms (38.10%), Space: 12.3 MB (64.88%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,23 +1,31 @@
-// tc : O(n^2)  - > not optimized solution 
+// tc : O(n)  - >  optimized solution
 
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int n = nums.size();
+                if (n == 0) return {}; // Handle the edge case of an empty array
+
         vector<int> res;
-        int ct;
+        long long int zor=0;
+        //* calculate zor all the elements
         for (int i = 0; i < n; i++) {
-              ct = 0;
-            for (int j = 0; j < n; j++) {
-              
-                if (nums[i] == nums[j]) {
-                    ct++;
-                }
+            zor = zor ^ nums[i];
+        }
+        int res1=0,res2=0;
+        // then i calculate the right most bit using xor property 
+        int num = (zor & ~(zor-1));
+        for (int i=0;i<n;i++){
+            if((nums[i]&num)!=0){
+                res1 = res1 ^ nums[i];
             }
-            if (ct % 2 != 0) {
-                res.push_back(nums[i]);
+            else {
+                res2 = res2 ^ nums[i];
             }
         }
+        res.push_back(res1);
+        res.push_back(res2);
         return res;
     }
 };
+
