62a36ed945f7c7d6d5227d90ca176bafaf71f0af
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-08 11:24:45 +0530

    Time: 2 ms (51.31%), Space: 7.4 MB (80.7%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        if(x==0){
+            return 0;
+        }
+        long long int l=1;
+        long long int h=x;
+        long long int ans=-1;
+        while(l<=h){
+            long long int mid = (l+h)/2;
+           long long int msq = mid*mid;
+            if(x==msq){
+                return mid;
+            }
+            else if (msq>x){
+                h = mid-1;
+
+            }
+            else{
+                l = mid+1;
+                ans = mid;
+            }
+        }
+        return (int)ans;
         
     }
 };
