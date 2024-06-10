36f8ae592d0eb50b015056647dc28df9f152166c
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-10 14:08:21 +0530

    Time: 23 ms (69.1%), Space: 94.4 MB (73.88%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,40 @@
-
-
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-        int m = B.size();
-        if (n > m)
-            return findMedianSortedArrays(B, A);
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-        int start = 0;
-        int end = n;
-
-        while (start <= end) {
-            int i1 = (start + end) / 2;
-            int i2 = ((n + m + 1) / 2) - i1;
-
-            long long max1 = (i1 == 0) ? INT_MIN : A[i1 - 1];
-            long long max2 = (i2 == 0) ? INT_MIN : B[i2 - 1];
-            long long min1 = (i1 == n) ? INT_MAX : A[i1];
-            long long min2 = (i2 == m) ? INT_MAX : B[i2];
-
-            if (max1 <= min2 && max2 <= min1) {
-                if ((m + n) % 2 == 0)
-                    return (double)(max(max1, max2) + min(min1, min2)) / 2;
-                else
-                    return (double)max(max1, max2);
-            }
-            else if (max1 > min2) {
-                end = i1 - 1;
-            }
-            else {
-                start = i1 + 1;
-            }
+        int n = nums1.size();
+        int m = nums2.size();
+        if(n>m){
+            return findMedianSortedArrays(nums2,nums1);
         }
 
+        int  l=0;
+        int h = n;
+        while(l<=h){
+            int i1 = (l+h)/2;
+            int i2 = ((n+m+1)/2)-i1;
+            int max1 = (i1==0) ? INT_MIN : nums1[i1-1];
+            int max2 = (i2==0) ? INT_MIN :  nums2[i2-1];
+            int min1 = (i1==n) ? INT_MAX : nums1[i1];
+            int min2 = (i2==m) ? INT_MAX : nums2[i2];
+
+            if(max1<=min2&& max2<=min1){
+                if((m+n)%2==0){
+                    return  (double) (max(max1,max2)+min(min1,min2))/2;
+
+                }
+                else{
+                    return (double) max(max1,max2);
+                }
+            }
+            else if(max1>min2){
+                h = i1-1;
+            }
+            else{
+                l= i1+1;
+            }
+        }
         return 0.0;
+        
     }
 };
