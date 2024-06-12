4bcf2ae1775f9e2e67daca51b813f31238c234fe
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-12 11:48:20 +0530

    Time: 29 ms (45.62%), Space: 38.2 MB (54.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(const std::vector<int>& nums) {
-    int n = nums.size();
-    std::vector<int> result(n, 1);
-    
-    // Create left product array
-    for (int i = 1; i < n; ++i) {
-        result[i] = result[i - 1] * nums[i - 1];
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int> res(n, 1);
+
+        // * create left product array
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        // Create right product and combine with left product
+        int right_product = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right_product;
+            right_product *= nums[i];
+        }
+        return res;
+
     }
-    
-    // Create right product and combine with left product
-    int rightProduct = 1;
-    for (int i = n - 1; i >= 0; --i) {
-        result[i] *= rightProduct;
-        rightProduct *= nums[i];
-    }
-    
-    return result;
-}
 };
