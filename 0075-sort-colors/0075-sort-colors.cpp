fdecb590c80b206a18b106cf9c7ece55af876cef
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-12 08:01:16 +0530

    Time: 5 ms (17.05%), Space: 9.7 MB (71.79%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 0; i < n - 1; i++) {
+            // Last i elements are already in place
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element found is greater than the next element
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+};
