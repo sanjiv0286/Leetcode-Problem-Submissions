1d5d9ad243518951d6576593174c744a08a2d689
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-13 07:57:39 +0530

    Time: 10 ms (22.69%), Space: 22.3 MB (17.35%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& se, vector<int>& st) {
-        sort(se.begin(), se.end());
-        sort(st.begin(), st.end());
-        int n = se.size();
-        int ans=0;
-        for (int i = 0; i < n; i++) {
-            int diff = abs(se[i] - st[i]);
-            ans += diff;
-        }
-        return ans;
+      priority_queue<int>pq1,pq2;// O(1)
+      int n = se.size();
+      // O(nlog(n))
+      for(int i=0;i<n;i++){
+        pq1.push(se[i]);
+        pq2.push(st[i]);
+      }
+      int ans =0;
+      for(int i=0;i<n;i++){
+          int a = pq1.top();
+          int b = pq2.top();
+          pq1.pop();
+          pq2.pop();
+          ans += abs(a-b);
+      }
+      return ans ;
     }
 };
