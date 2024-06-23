d077482009179f0b7f164410ef3f90f2c66ad68b
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-23 10:15:10 +0530

    Time: 6 ms (28.57%), Space: 25.7 MB (28.57%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     double minimumAverage(vector<int>& nums) {
-
+        sort(nums.begin(), nums.end());
+        deque<int> dq;
         int n = nums.size();
-        vector<double>res;
-        while(nums.size()>0){
-            auto  mini = min_element(nums.begin(),nums.end());
-            auto  maxi = max_element(nums.begin(),nums.end());
-            double avg = (*mini +*maxi)/2.0;
-            res.push_back(avg);
-            nums.erase(mini);
-            if(nums.size()>0){
-                auto  it3 = max_element(nums.begin(),nums.end());
-                nums.erase(it3);
-            }
-
+        for (int i = 0; i < n; i++) {
+            dq.push_back(nums[i]);
         }
-        return *min_element(res.begin(),res.end());
+        int i = 0;
+        int op = n / 2;
+        double ans = INT_MAX;
+        while (i < op) {
+            i++;
+            int ele1 = dq.front();
+            int ele2 = dq.back();
+            dq.pop_front();
+            dq.pop_back();
+            double avg = (ele1 + ele2) / 2.0;
+            ans = min(ans, avg);
+        }
+        return ans;
     }
 };
