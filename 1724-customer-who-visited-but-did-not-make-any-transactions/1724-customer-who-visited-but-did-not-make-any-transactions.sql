e0d26eef2956ef13f66f372c83c054b9fed2fed2
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-27 10:32:51 +0530

    Time: 1227 ms (84.43%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,22 +1,3 @@
 select v.customer_id ,count(v.visit_id) as count_no_trans from visits as v left join transactions as t on v.visit_id = t.visit_id where transaction_id is null group by customer_id  order by customer_id;
 
 
--- **************************************************************
-
--- SELECT
---     v.customer_id,
---     COUNT(v.visit_id) AS count_no_trans
--- FROM
---     Visits v
--- WHERE
---     v.visit_id NOT IN (
---         SELECT
---             t.visit_id
---         FROM
---             Transactions t
---         NATURAL JOIN Visits v2
---     )
--- GROUP BY
---     v.customer_id
--- ORDER BY
---     v.customer_id;  -- You can order by any field you prefer, or not order at all
