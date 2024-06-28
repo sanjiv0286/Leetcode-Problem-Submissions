586a5ee6f2670ee950acb422ac5b45fe223bd900
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-28 17:33:58 +0530

    Time: 317 ms (91.58%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,10 @@
+select 
+    query_name,
+    round(avg(rating / position), 2) as quality,
+    round(100.0 * sum(case when rating < 3 then 1 else 0 end) / count(*), 2) as poor_query_percentage
+from 
+    queries
+where 
+    query_name is not null
+group by 
+    query_name;
