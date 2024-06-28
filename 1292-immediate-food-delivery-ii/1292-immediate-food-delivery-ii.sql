b3784c62273c0b97520c87a4269852c5755d80e2
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-28 20:50:14 +0530

    Time: 617 ms (75.58%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,21 +1,17 @@
-
-select round(sum(if (min_order_date=min_customer_pref_delivery_date,1,0)*100)/count(min_order_date) ,2) as immediate_percentage  from 
-(select delivery_id , customer_id , min(order_date) as  min_order_date , min(customer_pref_delivery_date) as min_customer_pref_delivery_date from delivery group by customer_id) as new_table ;
-
--- ************************************************
--- select 
---     round(
---         sum(if(min_order_date = min_customer_pref_delivery_date, 1, 0)) * 100.0 / count(min_order_date), 
---         2
---     ) as immediate_percentage
--- from 
---     (
---         select 
---             customer_id,
---             min(order_date) as min_order_date,
---             min(customer_pref_delivery_date) as min_customer_pref_delivery_date
---         from 
---             delivery
---         group by 
---             customer_id
---     ) as new_table;
+with first as (
+    select 
+        delivery_id,
+        customer_id,
+        min(order_date) as min_order_date,
+        min(customer_pref_delivery_date) as min_customer_pref_delivery_date
+    from 
+        delivery
+    group by 
+        customer_id
+)
+select 
+    round(
+        sum(case when min_order_date = min_customer_pref_delivery_date then 1 else 0 end) * 100.0 / count(min_order_date), 2
+    ) as immediate_percentage
+from 
+    first;
