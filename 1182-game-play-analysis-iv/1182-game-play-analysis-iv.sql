c8cc4bca23796c2b6782d31f4d58a417f899ba24
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-29 09:22:49 +0530

    Time: 479 ms (97.27%), Space: 0B (100%) - LeetHub

---
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -0,0 +1,2 @@
+select round(sum(login_diff)/count(distinct player_id),2) as fraction from
+(select player_id,datediff(event_date, min(event_date) over (partition by player_id))=1 as login_diff from activity ) as game ;
