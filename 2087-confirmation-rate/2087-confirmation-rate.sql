26286696f62c54995c49f4e1274289ed4cff15c1
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-06-30 18:15:00 +0530

    Time: 557 ms (92.56%), Space: 0B (100%) - LeetHub

---
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -0,0 +1,18 @@
+SELECT 
+    s.user_id, 
+    ROUND(
+        IFNULL(
+            SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / 
+            SUM(CASE WHEN c.action IN ('confirmed', 'timeout') THEN 1 ELSE 0 END),
+            0
+        ),
+        2
+    ) AS confirmation_rate
+FROM 
+    signups s
+LEFT JOIN 
+    confirmations c 
+ON 
+    s.user_id = c.user_id
+GROUP BY 
+    s.user_id;
