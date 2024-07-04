c6c728cbaf54513365f60fabb9fd6fb7faacd72f
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-04 23:52:30 +0530

    Time: 6 ms (63.43%), Space: 16.5 MB (97.23%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int r, int c, int color, int newColor,
+    void dfs(vector<vector<int>>& image, int r, int c, int currColor, int given_color,
              int n, int m) {
-        if (r < 0 || r > n - 1 || c < 0 || c > m - 1 ||
-            image[r][c] != color) {
+        if (r < 0 || r > n - 1 || c < 0 || c > m - 1 || image[r][c] != currColor) {
             return;
         }
-
-        image[r][c] = newColor;
-
-        dfs(image, r - 1, c, color, newColor, n, m);
-        dfs(image, r + 1, c, color, newColor, n, m);
-        dfs(image, r, c - 1, color, newColor, n, m);
-        dfs(image, r, c + 1, color, newColor, n, m);
+        image[r][c] = given_color;
+        dfs(image, r - 1, c, currColor, given_color, n, m);
+        dfs(image, r + 1, c, currColor, given_color, n, m);
+        dfs(image, r, c - 1, currColor, given_color, n, m);
+        dfs(image, r, c + 1, currColor, given_color, n, m);
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int new_color) {
+                                  int given_color) {
         if (image.empty()) {
             return image;
         }
-        int Color = image[sr][sc];
-        // int new_color = color;
         int n = image.size();
         int m = image[0].size();
-       if(Color!=new_color){
-             dfs(image, sr, sc, Color, new_color, n, m);
+        int currColor = image[sr][sc];
+        if (currColor != given_color) {
+            dfs(image, sr, sc, currColor, given_color, n, m);
         }
         return image;
     }
