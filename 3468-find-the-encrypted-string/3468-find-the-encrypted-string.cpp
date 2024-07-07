5649e365fc56eb64c29f3540e228501d84d08b14
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-07 12:14:22 +0530

    Time: 0 ms (100%), Space: 8 MB (16.67%) - LeetHub

---
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string getEncryptedString(string s, int k) {
+
+        int n = s.size();
+        string res = s;
+        for (int i = 0; i < n; i++) {
+            res[i] = s[(i + k) % n];
+        }
+        return res;
+    }
+};
