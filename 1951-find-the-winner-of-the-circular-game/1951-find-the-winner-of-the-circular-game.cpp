9694ca4d0d0adad20e44bb41f43e29353b2d72e6
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-08 11:11:07 +0530

    Time: 0 ms (100%), Space: 7.2 MB (62.2%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+   int jos(int n,int k){
+      if(n==1){
+        return 0;
+      }
+      return (jos(n-1,k)+k)%n;
+   }
+
+
+    int findTheWinner(int n, int k) {
+        return jos(n,k)+1;
+    }
+};
