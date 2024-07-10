302fda9ec013dd4d11b3449ce45a9c328e846c0f
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-10 17:30:56 +0530

    Time: 672 ms (26.1%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -2,6 +2,13 @@
 -- union 
 -- select * from patients where  conditions  like "% diab1%"
 
-SELECT *
-FROM Patients
-WHERE conditions REGEXP '\\bDIAB1';
+SELECT *  FROM Patients WHERE conditions REGEXP '\\bDIAB1';
+
+
+-- \\b: This is a word boundary anchor. it ensures that "DIAB1" is treated as a distinct word or prefix.
+
+-- So, '\\bDIAB1' together matches "DIAB1" at the beginning of a word or as a standalone word. This ensures that "DIAB1" is not part of another string like "ADIAB1C".
+
+-- "DIAB100": Matches because "DIAB1" is at the beginning of the word.
+-- "ACNE DIAB100": Matches because "DIAB1" is at the beginning of the word "DIAB100".
+-- "YFEV DIAB1 MYOP": Matches because "DIAB1" is a standalone word.
