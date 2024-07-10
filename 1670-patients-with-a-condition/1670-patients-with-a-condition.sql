65acf867486b4441cae9f4cf21dac6698534eb22
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-10 17:22:20 +0530

    Time: 306 ms (81.52%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,5 +1,7 @@
--- select * from patients where  conditions  like "%DIAB1%"
-select * from patients where  conditions  like "diab1%" 
-union 
-select * from patients where  conditions  like "% diab1%"
+-- select * from patients where  conditions  like "DIAB1%" 
+-- union 
+-- select * from patients where  conditions  like "% diab1%"
 
+SELECT *
+FROM Patients
+WHERE conditions REGEXP '\\bDIAB1';
