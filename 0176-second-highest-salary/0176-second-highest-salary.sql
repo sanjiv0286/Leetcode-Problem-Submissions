5634b127159f5512f015245b84c0bf3408ec923b
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-11 11:47:10 +0530

    Time: 775 ms (6.39%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,1 +1,3 @@
-select max(salary) as SecondHighestSalary from employee where salary not in (select max(salary) from employee );
+-- select max(salary) as SecondHighestSalary from employee where salary not in (select max(salary) from employee );
+
+select max(e1.salary) as SecondHighestSalary from employee as e1  where 1 = (select count(distinct e2.salary ) from employee as e2 where e2.salary>e1.salary);
