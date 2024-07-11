6bb80f3f9c64860c18ec6c28a0535c863de72805
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-11 17:04:26 +0530

    Time: 713 ms (58.08%), Space: 0B (100%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -1,5 +1,6 @@
-select user_id,name,mail
-from users
-where
-regexp_like (mail,'^[a-z]+[0-9a-zA-Z_.-]*@leetcode[.]{1}com$')
+-- select user_id,name,mail
+-- from users
+-- where
+-- regexp_like (mail,'^[a-z]+[0-9a-zA-Z_.-]*@leetcode[.]{1}com$')
 
+select * from Users where right(mail, 13) like "@leetcode.com" and left(mail, length(mail)-13) rlike '^[A-Za-z][A-Za-z0-9._-]*$'
