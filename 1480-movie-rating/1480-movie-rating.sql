e2e0cca82c38d949b7cb2302dbb802c20b1eedac
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-12 21:58:13 +0530

    Time: 1070 ms (81.34%), Space: 0B (100%) - LeetHub

---
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -0,0 +1,13 @@
+(select u.name as results  from users as u left join MovieRating as mr on u.user_id = mr.user_id group by mr.user_id order by count(mr.user_id) desc, u.name  limit 1 )
+
+union all
+
+(select m.title as results
+from movies m
+left join MovieRating mr
+on m.movie_id= mr.movie_id
+where extract(year_month from created_at) = 202002
+group by m.movie_id
+order by avg(mr.rating) desc ,m.title
+limit 1
+)
