2d618d16a9bad391f612fd6bb9f58d1c34db7154
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-13 17:55:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
-        // move all the nonzero elements advance
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] ) {
-                nums[j++] = nums[i];
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+               nums.erase(nums.begin()+i);
+               nums.push_back(0);
             }
         }
-        for (int k =j;k < nums.size(); k++) {
-            nums[k] = 0;
-        }
+        
     }
 };
