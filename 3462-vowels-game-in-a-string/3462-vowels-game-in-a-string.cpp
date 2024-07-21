98d1666aa38b98ba5249a125c740625802eaece5
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-21 13:55:49 +0530

    Time: 50 ms (40%), Space: 26.5 MB (20%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,11 +1,44 @@
+#include <string>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
+    bool isvowel(char a) {
+        return a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u';
+    }
+    
     bool doesAliceWin(string s) {
-        int cnt=0;
-        for(auto i:s){
-            if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u')cnt++;
+        int oddVowelSubstrings = 0;
+        int evenVowelSubstrings = 0;
+        int n = s.size();
+        vector<int> vowelPrefixSum(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            vowelPrefixSum[i + 1] = vowelPrefixSum[i] + isvowel(s[i]);
         }
-        if(cnt==0)return 0;
-        return 1;;
+
+        for (int i = 0; i <= n; i++) {
+            if (vowelPrefixSum[i] % 2 == 0) {
+                evenVowelSubstrings++;
+            } else {
+                oddVowelSubstrings++;
+            }
+        }
+
+        return oddVowelSubstrings > 0;
     }
 };
+// **********************************************
+// class Solution {
+// public:
+//     bool doesAliceWin(string s) {
+//         int cnt=0;
+//         for(auto i:s){
+//             if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u')cnt++;
+//         }
+//         if(cnt==0)return 0;
+//         return 1;;
+//     }
+// };
+
