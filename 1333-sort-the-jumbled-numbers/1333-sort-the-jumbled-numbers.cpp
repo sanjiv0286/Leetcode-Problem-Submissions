cc939f12c3b36486d6b8feefd26784e36346815f
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-07-24 14:24:21 +0530

    Time: 515 ms (26.41%), Space: 88.9 MB (55.84%) - LeetHub

---
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        int n = mapping.size();
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[i] = mapping[i];
+        }
+        vector<pair<int, int>> res;
+        int m = nums.size();
+        for (int i = 0; i < m; i++) {
+            string s = "";
+            string temp = to_string(nums[i]);
+            for (int j = 0; j < temp.size(); j++) {
+                s += to_string(
+                    mp[temp[j] - '0']); // Convert character to integer index
+            }
+            int ele = stoi(s);
+            res.push_back({ele, i});
+        }
+        vector<int> ans;
+        sort(res.begin(), res.end());
+        for(auto &x:res){
+            ans.push_back(nums[x.second]);
+        }
+        return ans;
+    }
+};
