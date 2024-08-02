1b154cc6dafcb4a25bb76adf33c8da48d801da44
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-08-02 18:52:11 +0530

    Time: 66 ms (69.35%), Space: 90 MB (24.36%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-    int minSwaps(vector<int>& nums) {
-        int k = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        int cnt = accumulate(nums.begin(), nums.begin() + k, 0);
-        int mx = cnt;
-        for (int i = k; i < n + k; ++i) {
-            cnt += nums[i % n] - nums[(i - k + n) % n];
-            mx = max(mx, cnt);
+    int minSwaps(vector<int>& v) {
+        int n = v.size();
+        int ct = count(v.begin(), v.end(), 1);
+        vector<int> temp(2 * n);
+        for (int i = 0; i < 2 * n; i++) {
+            temp[i] = v[i % n];
         }
-        return k - mx;
+        int cntone = 0;
+        int maxi = 0;
+        int i = 0;
+        int j = 0;
+        while (j < 2 * n) {
+            if (temp[j] == 1) {
+                cntone++;
+            }
+            if (j - i + 1 > ct) {
+                cntone -= temp[i];
+                i++;
+            }
+            maxi = max(maxi, cntone);
+            j++;
+        }
+        return ct - maxi;
     }
 };
