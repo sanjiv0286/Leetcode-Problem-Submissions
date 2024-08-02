08d53752d7d89836707831dae30d8ee4c9667eb2
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-08-02 18:55:11 +0530

    Time: 71 ms (45.38%), Space: 83.1 MB (47.74%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,22 +1,53 @@
+// ***************************** O(2*n) ****************************************
+// class Solution {
+// public:
+//     int minSwaps(vector<int>& v) {
+//         int n = v.size();
+//         int ct = count(v.begin(), v.end(), 1);
+//         vector<int> temp(2 * n);
+//         for (int i = 0; i < 2 * n; i++) {
+//             temp[i] = v[i % n];
+//         }
+//         int cntone = 0;
+//         int maxi = 0;
+//         int i = 0;
+//         int j = 0;
+//         while (j < 2 * n) {
+//             if (temp[j] == 1) {
+//                 cntone++;
+//             }
+//             if (j - i + 1 > ct) {
+//                 cntone -= temp[i];
+//                 i++;
+//             }
+//             maxi = max(maxi, cntone);
+//             j++;
+//         }
+//         return ct - maxi;
+//     }
+// };
+
+// ****************************** O(n) ******************************************
+
 class Solution {
 public:
     int minSwaps(vector<int>& v) {
         int n = v.size();
         int ct = count(v.begin(), v.end(), 1);
-        vector<int> temp(2 * n);
-        for (int i = 0; i < 2 * n; i++) {
-            temp[i] = v[i % n];
-        }
+        // vector<int> temp(2 * n);
+        // for (int i = 0; i < 2 * n; i++) {
+        //     temp[i] = v[i % n];
+        // }
         int cntone = 0;
         int maxi = 0;
         int i = 0;
         int j = 0;
         while (j < 2 * n) {
-            if (temp[j] == 1) {
+            if (v[j%n] == 1) {
                 cntone++;
             }
             if (j - i + 1 > ct) {
-                cntone -= temp[i];
+                cntone -= v[i%n];
                 i++;
             }
             maxi = max(maxi, cntone);
