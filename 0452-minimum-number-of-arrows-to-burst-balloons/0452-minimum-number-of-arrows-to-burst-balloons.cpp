ed8070ebe193fef003ddedcb86dd6f3685a27c06
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-08-03 06:16:07 +0530

    Time: 242 ms (82.71%), Space: 93.6 MB (44.35%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    static bool com(const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1];
-    }
-
     int findMinArrowShots(vector<vector<int>>& points) {
-        if (points.empty()) {
-            return 0;
-        }
-        sort(points.begin(), points.end(), com);
-        // ***********Sort using a lambda comparator function
-        // sort(points.begin(), points.end(),
-        //      [](const vector<int>& a, const vector<int>& b) {
-        //          return a[1] < b[1];
-        //      });
-        int arrows = 1;
-        int end = points[0][1];
-        for (const auto& point : points) {
-            if (point[0] > end) {
-                arrows++;
-                end = point[1];
+        int n = points.size();
+        sort(begin(points), end(points),
+             [](vector<int>& v1, vector<int>& v2) { return v1[1] < v2[1]; });
+
+        int count = 1;
+        int lastEndPoint = points[0][1];
+
+        for (int i = 1; i < n; i++) {
+            int curr_startPoint = points[i][0];
+
+            if (curr_startPoint > lastEndPoint) {
+                count++;
+                lastEndPoint = points[i][1];
             }
         }
-        return arrows;
+
+        return count;
     }
 };
