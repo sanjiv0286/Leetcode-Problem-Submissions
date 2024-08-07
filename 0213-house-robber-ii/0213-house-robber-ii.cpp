582c474769e62f9c7d93b062d4ca7043d54c14ed
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-08-07 14:11:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (18.2%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,42 @@
-
-
+//Approach-2 (Using Bottom Up similar to House Robber-1 and just taking two cases
+/*
+    Case-1 (Take from 1st House - Hence skip the last house)
+    Case-2 (Take from 2nd House - Hence take the last house)
+*/
 class Solution {
 public:
-    int dp[101];
-
-    int solve(vector<int>& nums, int n, int i) {
-        if (i >= n)
-            return 0;
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int steal = nums[i] + solve(nums, n, i + 2);
-        int skip = solve(nums, n, i + 1);
-
-        return dp[i] = max(steal, skip);
-    }
-
+    int n;
+    
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
+        n = nums.size();
+        if(n == 1)
             return nums[0];
+        
+        vector<int> t(n+1, 0);
+        //t[i] = Max money gained from i houses
+        
+        int result1 = 0;
+        int result2 = 0;
+        
+        //Case-1 (Take from 1st House - Hence skip the last house)
+        t[0] = 0;
+        for(int i = 1; i<=n-1; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
         }
-        if (n == 2) {
-            return max(nums[0], nums[1]);
+        result1 = t[n-1];
+        
+        t.clear();
+        //Case-2 (Take from 2nd House - Hence take the last house)
+        t[0] = 0;
+        t[1] = 0;
+        for(int i = 2; i<=n; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
         }
-        memset(dp, -1, sizeof(dp));
-        int take_0th_house = solve(nums, n - 1, 0);
-        memset(dp, -1, sizeof(dp));
-        int take_1st_house = solve(nums, n, 1);
-        return max(take_0th_house, take_1st_house);
+        result2 = t[n];
+        
+        return max(result1, result2);
+        
     }
 };
+
+
