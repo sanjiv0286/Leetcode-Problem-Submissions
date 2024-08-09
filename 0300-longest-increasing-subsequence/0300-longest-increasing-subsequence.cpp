a1d54869ded25ab4552d380a69672ae626c03b2d
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-08-09 21:08:53 +0530

    Time: 9 ms (86.35%), Space: 12.9 MB (82.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,20 @@
+// tricky approach 
 class Solution {
 public:
-    int dp[2501][2501];
-    int solve(int i, int prev, vector<int>& nums) {
-        if (i == nums.size()) {
-            return 0;
-        }
-        if (prev != -1 && dp[prev][i] != -1)
-            return dp[prev][i];
-        int take = 0;
-        if (prev == -1 || nums[prev] < nums[i]) {
-            take = 1 + solve(i + 1, i, nums);
-        }
-        int skip = solve(i + 1, prev, nums);
-        if (prev != -1)
-            dp[prev][i] = max(take, skip);
-        return max(take, skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(0, -1, nums);
+        vector<int>sorted;
+
+        for(int i=0;i<n;i++){
+            auto it = lower_bound(sorted.begin(),sorted.end(),nums[i]);
+            if(it==sorted.end()){
+                sorted.push_back(nums[i]);
+            }
+            else{
+                *it = nums[i];
+            }
+        }
+        return sorted.size();
+        
     }
 };
