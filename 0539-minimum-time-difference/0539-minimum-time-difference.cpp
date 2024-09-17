46ed358adff878fd42a5506d52e4a0d7003ceec3
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-09-17 10:48:29 +0530

    Time: 7 ms (95.61%), Space: 17.5 MB (76.85%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     int findMinDifference(vector<string>& t) {
         int mini = INT_MAX;
-        vector<int>time_min;
-        for(auto &x:t){
-            int hr = 10*(x[0]-'0')+(x[1]-'0');
-            int min = 10*(x[3]-'0')+(x[4]-'0');
-            time_min.push_back(hr*60+min);
+        vector<int> time_min;
+        for (auto& x : t) {
+            int hr = 10 * (x[0] - '0') + (x[1] - '0');
+            int min = 10 * (x[3] - '0') + (x[4] - '0');
+            time_min.push_back(hr * 60 + min);
         }
         int n = time_min.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int diff = abs(time_min[i]-time_min[j]);
-                mini = min({mini,diff,1440-diff});
-            }
+        
+        sort(time_min.begin(), time_min.end());
+
+        // Calculate the minimum difference between adjacent times
+        for (int i = 1; i < n; i++) {
+            mini = min(mini, time_min[i] - time_min[i - 1]);
         }
+
+        // Also, check the difference between the first and last time,
+        // considering the 24-hour wraparound
+        mini = min(mini, 1440 - (time_min[n - 1] - time_min[0]));
+
         return mini;
-
-
     }
 };
