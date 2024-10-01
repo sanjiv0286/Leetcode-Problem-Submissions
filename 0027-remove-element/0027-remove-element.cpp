e7d97da2682bfd34ea039fd07448701c6857e772
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-10-01 14:22:01 +0530

    Time: 2 ms (44.29%), Space: 10.7 MB (37.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int i = 0;  // Pointer to place the next element not equal to val.
+        int i = 0;
         for (int j = 0; j < nums.size(); j++) {
             if (nums[j] != val) {
                 nums[i] = nums[j];
