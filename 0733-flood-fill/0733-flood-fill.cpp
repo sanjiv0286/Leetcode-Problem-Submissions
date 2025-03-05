37da16ba38510de33c036e63b78709567e2f10ac
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-03-05 14:37:36 +0530

    Time: 0 ms (100%), Space: 18.2 MB (60.29%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int r, int c, int currColor, int given_color,
-             int n, int m) {
-        if (r < 0 || r > n - 1 || c < 0 || c > m - 1 || image[r][c] != currColor) {
-            return;
+    void dfs(int a, int b, int n, int m, vector<vector<int>>& ans, int drow[],
+             int dcol[], int sr, int sc,vector<vector<int>>& image) {
+        ans[sr][sc] = b;
+        for (int i = 0; i < 4; i++) {
+            int nrow = sr + drow[i];
+            int ncol = sc + dcol[i];
+            if (nrow >= 0 && nrow < n &&  ncol >= 0 && ncol < m &&
+                image[nrow][ncol] == a && ans[nrow][ncol] != b) {
+                dfs(a, b, n, m, ans, drow, dcol, nrow, ncol,image);
+            }
         }
-        image[r][c] = given_color;
-        dfs(image, r - 1, c, currColor, given_color, n, m);
-        dfs(image, r + 1, c, currColor, given_color, n, m);
-        dfs(image, r, c - 1, currColor, given_color, n, m);
-        dfs(image, r, c + 1, currColor, given_color, n, m);
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int given_color) {
-        if (image.empty()) {
-            return image;
-        }
+                                  int color) {
+        int intial_color = image[sr][sc];
+        int new_color = color;
         int n = image.size();
         int m = image[0].size();
-        int currColor = image[sr][sc];
-        if (currColor != given_color) {
-            dfs(image, sr, sc, currColor, given_color, n, m);
-        }
-        return image;
+        int drow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, +1, 0, -1};
+        vector<vector<int>> ans = image;
+        dfs(intial_color, new_color, n, m, ans, drow, dcol, sr, sc,image);
+        return ans;
     }
 };
