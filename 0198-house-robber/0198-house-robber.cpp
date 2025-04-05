4161d8e3b67d16c481c457b7a3279b3a1dfe57b6
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-04-05 12:51:42 +0530

    Time: 0 ms (100%), Space: 10.2 MB (70.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,26 +1,21 @@
-//Approach-3 (Space Optimized Bottom Up - Converting Approach-2)
-
 class Solution {
 public:
+    int dp[101];
+    int solve(vector<int>& nums, int i, int n) {
+        if (i >= n)
+            return 0;
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int steal = nums[i] + solve(nums, i + 2, n);
+        int skip = solve(nums, i + 1, n);
+        return dp[i]=max(steal, skip);
+    }
 
     int rob(vector<int>& nums) {
-        // memset(dp, 0, sizeof(dp));
+        int i = 0;
+        memset(dp,-1,sizeof(dp));
         int n = nums.size();
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return nums[0];
-        }
-      int  prevprev  = 0,  prev = nums[0];
-        for (int i = 2; i <= n; i++) {
-            int steal = nums[i - 1] + prevprev;
-            int skip = prev;
-           int  temp = max(steal, skip);
-            prevprev=prev;
-            prev = temp;
-        }
-
-        return prev;
+        return solve(nums, i, n);
     }
 };
