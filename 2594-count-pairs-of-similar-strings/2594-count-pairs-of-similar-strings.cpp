0fb4050a8a20d53a42408af99dfb4decc91cc3f1
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-04-13 01:19:36 +0530

    Time: 51 ms (32.54%), Space: 27.3 MB (40.32%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.cpp
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.cpp
@@ -1,17 +1,15 @@
+
 class Solution {
 public:
     int similarPairs(vector<string>& words) {
         unordered_map<string, int> freq;
         int count = 0;
-        for (auto& word : words) {
-            vector<int> present(26, 0);
-            for (char c : word)
-                present[c - 'a'] = 1;
 
-            string key;
-            for (int i = 0; i < 26; ++i) {
-                if (present[i])
-                    key += (char)(i + 'a');
+        for (string word : words) {
+            set<char> uniqueChars(word.begin(), word.end());
+            string key = "";
+            for (char c : uniqueChars) {
+                key += c;
             }
 
             count += freq[key];
