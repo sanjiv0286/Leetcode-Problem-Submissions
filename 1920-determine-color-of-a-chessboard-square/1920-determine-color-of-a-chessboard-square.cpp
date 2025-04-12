4089f2365dc7faf33efe0ae11a782759eacd30d0
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-04-12 16:07:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (41.55%) - LeetHub

--- 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.cpp
+++ 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     bool squareIsWhite(string s) {
-        vector<vector<int>> v(9, vector<int>(9, 0));
 
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
-                v[i][j] = (i + j) % 2;
-            }
-        }
-        int idx1 = (s[0] - 'a' + 1);
-        int idx2 = s[1] - '0';
-        return (v[idx1][idx2] == 1);
+        int r = s[0] - 'a'+1;
+        int c = s[1] - '0';
+        return (r+c)%2 == 1;
+        
     }
 };
