7a07cd7cec0ecaebddac7cc29b71f3f0b173ba73
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-06-23 09:47:30 +0530

    Time: 31 ms (89.82%), Space: 105.9 MB (61.25%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,38 +1,23 @@
+#define ll long long int
 class Solution {
 public:
-    int bsearch(vector<int>& nums, int n, int k, int i,
-                vector<long long>&prefix) {
-        int l = 0;
-        int r = i;
-        int res = 0;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            int ct = i - mid + 1;
-            long long windsum = (long long) ct * nums[i];
-            long long currsum = prefix[i] - prefix[mid] + nums[mid];
-            long long opr = windsum - currsum;
-            if (opr > k) {
-                l = mid + 1;
-            } else {
-                res = mid;
-                r = mid - 1;
-            }
-        }
-        return i - res + 1;
-    }
     int maxFrequency(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<long long> prefix(n);
-        prefix[0]= nums[0];
+        ll n = nums.size();
         sort(nums.begin(), nums.end());
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + nums[i];
+        ll res = 0;
+        ll l = 0;
+        ll cs = 0;
+        for (ll r = 0; r < n; r++) {
+            ll ws = (r - l + 1) * nums[r];
+            cs = cs + nums[r];
+            ll op = ws - cs;
+            while (op > k) {
+                cs -= nums[l];
+                l++;
+                op = (ll)(r - l + 1) * nums[r] - cs;
+            }
+            res = max(res, r - l + 1);
         }
-        int res = 1;
-        for (int i = 0; i < n; i++) {
-            int freq = bsearch(nums, n, k, i, prefix);
-            res = max(res, freq);
-        }
-        return res;
+        return (int)res;
     }
 };
