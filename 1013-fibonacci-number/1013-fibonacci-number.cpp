081790e0c4785dd8354cacde331aaf51460faa26
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-06-23 19:41:55 +0530

    Time: 0 ms (100%), Space: 8 MB (21.63%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,28 +1,38 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if (n == 0) {
+//             return 0;
+//         }
+//         if (n == 1) {
+//             return 1;
+//         }
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// };
+
+
 class Solution {
 public:
-    int dp[31];
-    int solve(int n) {
-        
-        if (n == 0) {
+    int solve(int n, vector<int>&dp) {
+        if (n == 0)
             return 0;
-        }
-        if (n == 1) {
+        if (n == 1)
             return 1;
-        }
-        if(dp[n]!=-1){
+        if (dp[n] != -1)
             return dp[n];
-        }
-        return dp[n]=solve(n - 1) + solve(n - 2);
-    }
-    int fib(int n) {
-        memset(dp, -1, sizeof(dp));
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
 
-        return solve(n);
+        return dp[n] = solve(n - 2,dp) + solve(n - 1,dp);
+    }
+
+    int fib(int n) {
+        vector<int> dp(31, -1);
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return solve(n, dp);
+
     }
 };
