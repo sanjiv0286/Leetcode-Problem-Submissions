1f88e2770631b44a1327c567d5e97b85480124cf
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-06-24 08:51:00 +0530

    Time: 4 ms (34.19%), Space: 10.8 MB (16.22%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+         if(x<0){
+            return false;
+        }
+        string s = to_string(x);
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (!isalnum(s[l])) {
+                l++;
+            } else if (!isalnum(s[r])) {
+                r--;
+            } else if (s[l] != s[r]) {
+                return false;
+            } else {
+                l++;
+                r--;
+            }
+        }
+        return true;
+       
+    }
+};
