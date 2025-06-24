4af48b821727a8b3bb659a409775bb093ab26503
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-06-24 08:22:45 +0530

    Time: 3 ms (28.84%), Space: 8.4 MB (82.14%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -8,9 +8,10 @@ public:
             if ((revnum > INT_MAX / 10) || (revnum < INT_MIN / 10)) {
                 return 0;
             }
-            revnum = revnum * 10 + d;
+           revnum = revnum * 10 + d;
             x /= 10;
         }
+       
 
         return revnum;
     }
