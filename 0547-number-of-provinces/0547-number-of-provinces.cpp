20c3cd71c38589f134a833e674012e0047ce8120
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-09-13 05:28:54 +0530

    Time: 0 ms (100%), Space: 21.5 MB (8.31%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-
-    void dfs(int u,unordered_map<int,vector<int>>&adj,vector<bool>&vis){
-           vis[u]=true;
-           for(auto &v:adj[u]){
+    void bfs( unordered_map<int,vector<int>>&adj,int s,  vector<bool>&vis){
+        queue<int>q;
+        q.push(s);
+        vis[s]=true;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            // cout<<u<<endl;
+            for(auto &v:adj[u]){
                 if(!vis[v]){
-                    dfs(v,adj,vis);
+                    vis[v]=true;
+                    q.push(v);
                 }
-           }
+            }
+        }
     }
     
     int findCircleNum(vector<vector<int>>& isConnected) {
+        int  n = isConnected.size();
         unordered_map<int,vector<int>>adj;
-        int n = isConnected.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1){
@@ -24,12 +31,11 @@ public:
         int ct=0;
         vector<bool>vis(n,false);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
+            if(vis[i]==false){
+                bfs(adj,i,vis);
                 ct++;
             }
         }
         return ct;
-
     }
 };
