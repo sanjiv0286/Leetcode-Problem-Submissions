0a86bb18c9a777591e00cc8337131318a592145b
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-09-13 04:42:49 +0530

    Time: 8 ms (11.68%), Space: 20.9 MB (30.55%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& isConnected, int u, vector<bool>& vis) {
-        vis[u] = true;
+
+    void dfs(int u,unordered_map<int,vector<int>>&adj,vector<bool>&vis){
+           vis[u]=true;
+           for(auto &v:adj[u]){
+                if(!vis[v]){
+                    dfs(v,adj,vis);
+                }
+           }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        unordered_map<int,vector<int>>adj;
         int n = isConnected.size();
-        for (int v = 0; v < n; v++) {
-            if (vis[v] == false && isConnected[u][v] == 1) {
-                dfs(isConnected, v, vis);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
             }
         }
-        return;
-    }
-
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        // vector<vector<int>> adj(n);
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (isConnected[i][j] == 1) {
-        //             adj[i].push_back(j);
-        //             adj[j].push_back(i);
-        //         }
-        //     }
-        // }
-        int ct = 0;
-        vector<bool> vis(n, false);
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == false) {
-                dfs(isConnected, i, vis);
+        int ct=0;
+        vector<bool>vis(n,false);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,adj,vis);
                 ct++;
             }
         }
         return ct;
+
     }
 };
