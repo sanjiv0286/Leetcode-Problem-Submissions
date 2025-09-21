fcc54f6842912113fe9c7304c7ea3adc6604cf18
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-09-21 16:09:51 +0530

    Time: 19 ms (96.15%), Space: 23.1 MB (36.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,46 @@
-
 class Solution {
 public:
-    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid, int dx[],
+    void bfs(int i, int j, int n, int m, vector<vector<char>>& grid, int dx[],
              int dy[]) {
-        grid[i][j] = '0';
-        for (int k = 0; k < 4; k++) {
-            int newi = i + dx[k];
-            int newj = j + dy[k];
-            if (newi >= 0 && newi < m && newj >= 0 && newj < n &&
-                grid[newi][newj] == '1') {
-                dfs(newi, newj, n, m, grid, dx, dy);
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        grid[i][j] = '0'; // mark visited
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+ 
+            for (int k = 0; k < 4; k++) {
+                int newx = x + dx[k];
+                int newy = y + dy[k];
+                if (newx >= 0 && newx < m && newy >= 0 && newy < n &&
+                    grid[newx][newy] == '1') {
+                    grid[newx][newy] = '0'; // mark visited
+                    q.push({newx, newy});
+                }
             }
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty()) {
+        if (grid.empty())
             return 0;
-        }
+
         int dx[] = {-1, 0, 1, 0};
         int dy[] = {0, 1, 0, -1};
         int m = grid.size();
         int n = grid[0].size();
-        int ct = 0;
+        int count = 0;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(i, j, n, m, grid, dx, dy);
-                    ct++;
+                    bfs(i, j, n, m, grid, dx, dy);
+                    count++;
                 }
             }
         }
-        return ct;
+
+        return count;
     }
 };
