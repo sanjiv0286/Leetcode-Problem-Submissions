2e82f3c941782c656b5009e8e4338e6936ccb460
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-10-02 14:26:02 +0530

    Time: 51 ms (89.64%), Space: 53.2 MB (93.41%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
-    int atMostK(vector<int>& arr, int k) {
-        int n = arr.size();
-        int res = 0;
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
 
-        // pointers to mark the left and right boundary
-        int left = 0, right = 0;
+private:
+    int atMostK(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        int l = 0, res = 0;
 
-        // Frequency map
-        unordered_map<int, int> freq;
-        while (right < n) {
-            freq[arr[right]] += 1;
+        for (int r = 0; r < nums.size(); r++) {
+            if (count[nums[r]] == 0) {
+                k--; // new distinct element
+            }
+            count[nums[r]]++;
 
-            // If this is a new element in the window,
-            // decrement k by 1
-            if (freq[arr[right]] == 1)
-                k -= 1;
-
-            // Shrink the window until distinct element
-            // count becomes <= k
             while (k < 0) {
-                freq[arr[left]] -= 1;
-                if (freq[arr[left]] == 0)
-                    k += 1;
-                left += 1;
+                count[nums[l]]--;
+                if (count[nums[l]] == 0) {
+                    k++; // one distinct removed
+                }
+                l++;
             }
 
-            // add all possible subarrays to result
-            res += (right - left + 1);
-            right += 1;
+            res += (r - l + 1);
         }
         return res;
-    }
-
-    int exactlyK(vector<int>& arr, int k) {
-        return atMostK(arr, k) - atMostK(arr, k - 1);
-    }
-
-    int subarraysWithKDistinct(vector<int>& arr, int k) {
-        int ans = exactlyK(arr, k);
-        return ans;
     }
 };
