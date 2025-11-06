d9112b53b773abf1432de2594376ab4740a24a97
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-11-06 09:32:27 +0530

    Time: 0 ms (100%), Space: 8.6 MB (75.32%) - LeetHub

--- 4052-equal-score-substrings/4052-equal-score-substrings.cpp
+++ 4052-equal-score-substrings/4052-equal-score-substrings.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     bool scoreBalance(string s) {
         int n = s.size();
-        int sum1 = 0, sum2 = 0;
+        int total=0;
+        for (auto& x : s) {
+            total += x - 'a' + 1;
+        }
+        int left=0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                sum1 += s[j] - 'a' + 1;
-            }
-            for (int k = i + 1; k < n; k++) {
-                sum2 += s[k] - 'a' + 1;
-            }
-            if (sum2 == sum1) {
+            left += s[i] - 'a' + 1;
+            if (left == (total - left)) {
                 return true;
             }
-            sum1=0;
-            sum2=0;
         }
-
         return false;
     }
 };
