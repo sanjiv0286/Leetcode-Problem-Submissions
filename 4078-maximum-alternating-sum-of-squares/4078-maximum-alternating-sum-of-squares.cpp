7c3f73ab1e29bb87f22fe0c9ea16b7e363777f91
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-11-06 13:37:31 +0530

    Time: 55 ms (71.72%), Space: 176.1 MB (48.76%) - LeetHub

--- 4078-maximum-alternating-sum-of-squares/4078-maximum-alternating-sum-of-squares.cpp
+++ 4078-maximum-alternating-sum-of-squares/4078-maximum-alternating-sum-of-squares.cpp
@@ -7,24 +7,24 @@ public:
             x = abs(x);
         }
         sort(nums.begin(), nums.end(), greater<int>());
-        // int j = 0, k = 1;/
+        int k = (n + 1) / 2;
         long long sum = 0;
-        if (n % 2 == 0) {
-            for (int i = 0; i < n / 2; i++) {
-                sum += nums[i] * nums[i];
-            }
-            for (int i = n / 2; i < n; i++) {
-                sum -= nums[i] * nums[i];
-            }
+        // if (n % 2 == 0) {
+        for (int i = 0; i < k; i++) {
+            sum += nums[i] * nums[i];
         }
-        if (n % 2 != 0) {
-            for (int i = 0; i <= n / 2; i++) {
-                sum += nums[i] * nums[i];
-            }
-            for (int i = n / 2 + 1; i < n; i++) {
-                sum -= nums[i] * nums[i];
-            }
+        for (int i = k; i < n; i++) {
+            sum -= nums[i] * nums[i];
         }
+        // }
+        // if (n % 2 != 0) {
+        //     for (int i = 0; i <= n / 2; i++) {
+        //         sum += nums[i] * nums[i];
+        //     }
+        //     for (int i = n / 2 + 1; i < n; i++) {
+        //         sum -= nums[i] * nums[i];
+        //     }
+        // }
         return sum;
     }
 };
