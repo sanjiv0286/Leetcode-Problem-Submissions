47174c292ab835edc5e7aef5534e52ea24ddcc71
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-11-08 01:58:17 +0530

    Time: 0 ms (100%), Space: 50.5 MB (28.38%) - LeetHub

--- 4008-restore-finishing-order/4008-restore-finishing-order.cpp
+++ 4008-restore-finishing-order/4008-restore-finishing-order.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
-        int n = order.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[order[i]]=i;
-        }
+    vector<int> recoverOrder(vector<int>& o, vector<int>& f) {
+        unordered_set<int>st(f.begin(),f.end());
         vector<int>res;
-        for(auto &x:friends){
-            res.push_back(mp[x]);
-        }
-        sort(res.begin(),res.end());
-        for(int i=0;i<friends.size();i++){
-            res[i] = order[res[i]];
+        for(auto &x:o){
+            if(st.count(x)==1)
+            res.push_back(x);
         }
         return res;
-        
+
     }
 };
