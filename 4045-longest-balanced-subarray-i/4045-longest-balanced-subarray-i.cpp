19f91dbb238d3c6dc96de4c8c9d7a18868464fb0
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2025-11-11 11:52:27 +0530

    Time: 2027 ms (35.64%), Space: 522.2 MB (53.24%) - LeetHub

--- 4045-longest-balanced-subarray-i/4045-longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/4045-longest-balanced-subarray-i.cpp
@@ -3,9 +3,10 @@ public:
     int longestBalanced(vector<int>& nums) {
         int n = nums.size();
         int maxi = 0;
-        unordered_set<int> st;
         for (int i = 0; i < n; i++) {
-            st.clear();
+            unordered_set<int> st;
+
+            // st.clear();
             int even = 0;
             int odd = 0;
             for (int j = i; j < n; j++) {
