2cc81baee7b95d521577ae9b781934eb354ff4a0
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-01-20 08:47:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.38%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        return (__builtin_popcount(n));
+    int hammingWeight(int n) {
+
+        int x = __builtin_popcount(n);
+
+        return x;
         
     }
 };
