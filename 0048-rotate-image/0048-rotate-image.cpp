873d1b0ff0e0e11d99c2e6c71df58d8ad6557615
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-01-20 22:15:44 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.07%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,7 +5,7 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<m;j++){
+            for(int j=i;j<m;j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
