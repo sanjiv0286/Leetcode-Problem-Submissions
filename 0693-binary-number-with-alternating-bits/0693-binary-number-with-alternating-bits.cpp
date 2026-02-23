3a049f3297f37876cf6f86edeae49ac9891979bb
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-02-23 14:17:43 +0530

    Time: 0 ms (100%), Space: 8 MB (9.82%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -6,7 +6,7 @@ public:
         // return (ans > 0 && (ans & (ans + 1)) == 0);
         bitset<32>b(n);
         int hsb = log2(n);
-        for(int i=0;i<=hsb;i++){
+        for(int i=0;i<hsb;i++){
 
             if(b[i]==b[i+1]){
                 return false;
