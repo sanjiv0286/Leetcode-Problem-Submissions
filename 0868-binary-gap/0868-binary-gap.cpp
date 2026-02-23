8384340df0c7348be58721b9bc36054cada705a6
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-02-23 11:54:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.39%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -5,6 +5,11 @@ public:
         int curr = 0;
         int prev = -1;
         int res = 0;
+
+        if ( (n && (n - 1)) == 0 || n==1) {
+            return 0;
+        }
+
         // while(n){
         for ( curr = 0; curr < 32; curr++) {
             if (n & 1) {
