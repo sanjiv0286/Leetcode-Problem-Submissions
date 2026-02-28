25adb711c94758c8ad9a8bb1a68af17c4840eb3a
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-02-28 11:15:43 +0530

    Time: 54 ms (36.24%), Space: 8.1 MB (69.8%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -5,11 +5,12 @@ public:
         long long res = 0;
         for (int num = 1; num <= n; num++) {
             int bits = floor(log2(num)) + 1;
-            // res = (res*(1LL<<bits))%M;
-             res = ((res*(1LL<<bits))%M+num)%M;
-            // res = (res | num) % M;
+            //  res = ((res*(1LL<<bits))%M+num)%M;
+            res = (res*(1LL<<bits))%M;
+            res = (res + num) % M;
         }
-        // int bits = log2(14);
+        // int bits = log2(14); // this will give highest set bit index.
+        // if you find to find number of bits then use floor(log2(num))+1
         return int(res);
     }
 };
