93395c498cf2d9fb12293644e87afe69bc082136
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-02-28 11:32:04 +0530

    Time: 45 ms (56.38%), Space: 8 MB (89.26%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     const int M = 1e9 + 7;
     int concatenatedBinary(int n) {
+        int bits = 0;
         long long res = 0;
         for (int num = 1; num <= n; num++) {
-            int bits = floor(log2(num)) + 1;
-            //  res = ((res*(1LL<<bits))%M+num)%M;
-            res = (res*(1LL<<bits))%M;
-            res = (res + num) % M;
+            if ((num & (num - 1)) == 0) {
+                bits++;
+            }
+            // res = ((res * (1LL << bits)) % M + num) % M;
+            res = ((res << bits) % M + num) % M;
         }
-        // int bits = log2(14); // this will give highest set bit index.
-        // if you find to find number of bits then use floor(log2(num))+1
-        return int(res);
+        return res;
     }
 };
