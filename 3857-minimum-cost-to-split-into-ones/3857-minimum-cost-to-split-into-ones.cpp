28fb282a956a7faaf9c857849d3309be3019e6ce
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-03-01 18:31:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (28.15%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,11 +1,26 @@
+// class Solution {
+// public:
+//     int minCost(int n) {
+//         int cost = n * (n - 1);
+//         cost = cost / 2;
+
+//         return cost;
+//     }
+// };
+
 class Solution {
 public:
-    
- 
+    int dp[501];
+    int solve(int n) {
+        if (n == 1)
+            return 0;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=solve(n - 1) + (n - 1);
+    }
     int minCost(int n) {
-        int cost = n*(n-1);
-         cost = cost/2;
-
-        return cost;
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
