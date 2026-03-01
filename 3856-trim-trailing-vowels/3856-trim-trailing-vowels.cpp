499c3193a9e3b784b1741749851474840b9902bd
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-03-01 17:50:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (95.66%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    bool isVowel(char ch) {
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
-    }
-
     string trimTrailingVowels(string s) {
-        stack<char> st;
-        for (auto& x : s) {
-            st.push(x);
-        }
-        while (!st.empty()) {
-            if (isVowel(st.top())) {
-                st.pop();
+        while (!s.empty()) {
+            char ch = s.back();
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                s.pop_back();
             } else {
                 break;
             }
         }
-        string t="";
-        while (!st.empty()) {
-            t += st.top();
-            st.pop();
-        }
-        reverse(t.begin(), t.end());
-        return t;
+
+        return s;
     }
 };
