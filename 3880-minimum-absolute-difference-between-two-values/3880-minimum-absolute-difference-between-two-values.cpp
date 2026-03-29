e9e7448e6ac0864e29560e2e4f98a08f3b0f14e4
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-03-29 22:37:37 +0530

    Time: 3 ms (11.11%), Space: 26.8 MB (0%) - LeetHub

--- 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int minAbsoluteDifference(vector<int>& nums) {
-        vector<int> v1, v2;
+        unordered_map<int, vector<int>> mp;
         int n = nums.size();
         for (int i = 0; i < n; i++) {
-            if (nums[i] == 1) {
-                v1.push_back(i);
-            }
-            if (nums[i] == 2) {
-                v2.push_back(i);
-            }
+            mp[nums[i]].push_back(i);
         }
-        if(v1.size()==0|| v2.size()==0){
+        vector<int>& v1 = mp[1];
+        vector<int>& v2 = mp[2];
+        if (v1.size() == 0 || v2.size() == 0) {
             return -1;
         }
         int mini = INT_MAX;
-        for (int i = 0; i < v1.size(); i++) {
-            for (int j = 0; j < v2.size(); j++) {
-                mini = min(mini, abs(v1[i] - v2[j]));
+        for (int i : v1) {
+            for (int j : v2) {
+                mini = min(mini, abs(i - j));
             }
         }
         return mini;
