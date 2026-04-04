b1b65bceac65c51a4bfb69209b3b24cf510473ac
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-04-04 15:14:22 +0530

    Time: 3 ms (46.88%), Space: 9.7 MB (47.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
-        
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s == t) {
+            return true;
+        }
+        return false;
     }
 };
