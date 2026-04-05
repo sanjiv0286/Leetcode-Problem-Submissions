b3dccdf6410b76346c79879406e397bf4cb4afe2
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-04-05 10:20:13 +0530

    Time: 71 ms (63.37%), Space: 58 MB (87.5%) - LeetHub

--- 3889-mirror-frequency-distance/3889-mirror-frequency-distance.cpp
+++ 3889-mirror-frequency-distance/3889-mirror-frequency-distance.cpp
@@ -6,17 +6,17 @@ public:
         vector<int> alpha(26, 0);
         int n = s.size();
         for (int i = 0; i < n; i++) {
-            // if(isalpha(s[i])){
-            //     alpha[s[i]-'a']++;
-            // }
-            // else if (isdigit(s[i])){
-            //     digit[s[i]-'0']++;
-            // }
-            if (s[i] >= 'a' && s[i] <= 'z') {
-                alpha[s[i] - 'a']++;
-            } else if (s[i] >= '0' && s[i] <= '9') {
-                digit[s[i] - '0']++;
+            if(isalpha(s[i])){
+                alpha[s[i]-'a']++;
             }
+            else if (isdigit(s[i])){
+                digit[s[i]-'0']++;
+            }
+            // if (s[i] >= 'a' && s[i] <= 'z') {
+            //     alpha[s[i] - 'a']++;
+            // } else if (s[i] >= '0' && s[i] <= '9') {
+            //     digit[s[i] - '0']++;
+            // }
         }
         int ans = 0;
         for (int i = 0; i < 13; i++) {
