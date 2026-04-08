b5967ae3d402b642b49ae3841a1a660c996ea0a8
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-04-08 10:41:07 +0530

    Time: 11 ms (91.11%), Space: 24.8 MB (87.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,42 +1,16 @@
-// without sorting 
 class Solution {
 public:
-
-    string generate(string &s) {
-        // int count[26] = {0};
-        vector<int>freq(26,0);
-        for(char &ch : s) {
-            freq[ch-'a']++;
-        }
-
-        string new_s;
-
-        for(int i = 0; i<26; i++) {
-
-            if(freq[i] > 0) {
-                new_s += string(freq[i], i+'a');
-            }
-        }
-
-        return new_s;
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
-
-        for(string &str : strs) {
-            string s = generate(str);
-
-            mp[s].push_back(str);
+        for (auto& x : strs) {
+            string t = x;
+            sort(x.begin(), x.end());
+            mp[x].push_back(t);
         }
-
-        vector<vector<string>> result;
-        for(auto &x : mp) {
-            result.push_back((x.second));
+        vector<vector<string>> res;
+        for (auto& x : mp) {
+            res.push_back(x.second);
         }
-
-        return result;
-
+        return res;
     }
 };
-
