db2efdaf61c5965f9dc18eedaf1c21d00907c029
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2026-04-12 00:40:47 +0530

    Time: 0 ms (100%), Space: 18.1 MB (30.76%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,44 +1,22 @@
-
-// time complexity 
-// brute force : O(nlogn)
-// here : O(nlogk)
-// space : O(n+k)
 class Solution {
 public:
-    typedef pair<int, int> p;
-    // template <typename T>
-    // using min_heap = priority_queue<T, vector<T>, greater<T>>;
-    // Now you can declare a min heap of pairs of ints by writing:
-    // min_heap<pair<int, int>> q;
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int>res;
-        // 1. map
-        unordered_map<int, int> m;
+        unordered_map<int, int> mp;
         for (auto& x : nums) {
-            m[x]++;
+            mp[x]++;
+        }
+        vector<pair<int,int>>pa(mp.begin(),mp.end());
+        auto lamda = [](pair<int,int>& a, pair<int,int>& b) {
+            return a.second > b.second;
+        };
+        sort(pa.begin(), pa.end(), lamda);
+        vector<int>res;
+        for(auto &x:pa){
+            if(k>0)
+            res.push_back(x.first);
+            k--;
         }
 
-        // 2. min heap
-        priority_queue<p, vector<p>, greater<p>> pq;
-        // 3. push element in min heap
-        // and maintain size of k only
-        for(auto &x:m){
-            int first = x.first;
-            int second = x.second;
-            // understand push first second value in pq beacause pop easily acc. to key 
-            pq.push({second,first});
-            if(pq.size()>k){
-                pq.pop();
-            }
-        }
-        // 4.result 
-        while(!pq.empty()){
-            //   res.push_back(pq.top().first);
-            res.push_back(pq.top().second);//access second value
-            pq.pop();
-        }
         return res;
-
     }
 };
